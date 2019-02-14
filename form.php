fce6bfa2c0be085d014fd9c6dcb5527a1eb83de5
chlitapumxo <46773268+chlitapumxo@users.noreply.github.com>
2019-02-14 14:49:06 +0700

    Add files via upload

--- form.php
+++ form.php
@@ -12,20 +12,25 @@
     <fieldset id="sect1">
       <legend>ส่วนที่1 ข้อมูลผู้จัดทำโครงงาน </legend>
       <label for="sid">รหัสนักศึกษา</label><input type="text" name="sid">
-      <label for="program">สาขาวิชา</label>
+      <label for="program">สาขาวิชา</label> 
       <select name="program">
         <option value="1">วิทยาการคอมพิวเตอร์</option>
         <option value="2">เทคโนโลยีสารสนเทศ</option>
         <option value="3">เทคโนโลยีเครือข่ายคอมพิวเตอร์</option>
         <option value="4">ภูมิสารสนเทศ</option>
         <option value="5">คอมพิวเตอร์ศึกษา</option>
-      </select>
+      </select><br>
+      <label for="faculty">คณะ</label>
+      <select for="faculty">
+        <option value="1">วิทยาการคอมพิวเตอร์</option>
+        <option value="2">ครุศาสตร์</option>
+      </select><br>
       <label for="std_year">ชั้นปี</label><br>
-      <input type="radio" name="std_year" value="1"><label>ปี1</label><br>   
-      <input type="radio" name="std_year" value="1"><label>ปี2</label><br>   
-      <input type="radio" name="std_year" value="1"><label>ปี3</label><br>   
-      <input type="radio" name="std_year" value="1"><label>ปี4</label><br>   
-      <input type="radio" name="std_year" value="1"><label>ปี5</label><br>  
+      <input type="radio" name="std_year" value="1"><label>ปี1</label>
+      <input type="radio" name="std_year" value="1"><label>ปี2</label>  
+      <input type="radio" name="std_year" value="1"><label>ปี3</label>  
+      <input type="radio" name="std_year" value="1"><label>ปี4</label>   
+      <input type="radio" name="std_year" value="1"><label>ปี5</label>
     </fieldset>
     <form action="project_data.php" method="get">
     <fieldset id="sect1">
@@ -40,16 +45,16 @@
         <option value="4">โครงงานประเภทสิ่งประดิษฐ์</option>
       </select><br>
       <label>เครื่องมือที่ใช้ในการพัฒนา</label><br>
-      <input type="checkbox" name="prj_tool[]"><label>HTML</label><br>  
-      <input type="checkbox" name="prj_tool[]"><label>CSS</label><br>  
-      <input type="checkbox" name="prj_tool[]"><label>JavaScript</label><br>   
-      <input type="checkbox" name="prj_tool[]"><label>Bootstrap</label> <br> 
-      <input type="checkbox" name="prj_tool[]"><label>Materiai</label> <br>  
-      <input type="checkbox" name="prj_tool[]"><label>Angular</label> <br>  
-      <input type="checkbox" name="prj_tool[]"><label>React</label>  <br>  
-      <input type="checkbox" name="prj_tool[]"><label>PHP</label> <br> 
-      <input type="checkbox" name="prj_tool[]"><label>MySQL</label> <br>  
-      <input type="checkbox" name="prj_tool[]"><label>ChartJS</label> <br>  
+      <input type="checkbox" name="prj_tool[]"><label>HTML</label>
+      <input type="checkbox" name="prj_tool[]"><label>CSS</label>  
+      <input type="checkbox" name="prj_tool[]"><label>JavaScript</label>  
+      <input type="checkbox" name="prj_tool[]"><label>Bootstrap</label> 
+      <input type="checkbox" name="prj_tool[]"><label>Materiai</label>  
+      <input type="checkbox" name="prj_tool[]"><label>Angular</label>   
+      <input type="checkbox" name="prj_tool[]"><label>React</label>    
+      <input type="checkbox" name="prj_tool[]"><label>PHP</label> 
+      <input type="checkbox" name="prj_tool[]"><label>MySQL</label> 
+      <input type="checkbox" name="prj_tool[]"><label>ChartJS</label><br>  
       <label>อาจารย์ที่ปรึกษา</label>
       <select name="prj_advisor">
         <option value="1">ผศ.บุญเรือน พฤกษ์ศศิธร</option>
